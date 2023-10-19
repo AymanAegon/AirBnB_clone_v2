98450e12fc850ede24ce5e1cee1485c307f46d4d
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-19 18:55:28 +0100

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,5 +12,13 @@ app.url_map.strict_slashes = False
 def hello():
     return "Hello HBNB!"
 
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>")
+def ctext(text):
+    return "C {}".format(text)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
