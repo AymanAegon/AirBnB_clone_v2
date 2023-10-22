d84670b94b4aebb377c82a213eca79d731eec64a
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-22 18:57:42 +0100

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,12 +11,60 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
-def states_list():
+@app.route("/")
+def hello():
     """ function """
-    l = storage.all(State)
-    print(l)
-    return render_template("7-states_list.html")
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ function """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def ctext(text):
+    """ function """
+    return "C {}".format(text)
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def pytext(text="is cool"):
+    """ function """
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """ function """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """ function """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def number_odd_or_even(n):
+    """ function """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ def doc """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(error):
+    """ def doc """
+    storage.close()
 
 
 if __name__ == "__main__":
